c000d33c3b27f10fab4fa1422d584b068d41cf29
bleujaun <bleujaun@gmail.com>
2022-11-24 23:10:51 -0800

    Updates

---
+++ precache-manifest.abc95ca3085636fce21354cfa40fef95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388579a90592ef85b92571716155ff69",
+    "url": "/007/index.html"
+  },
+  {
+    "revision": "cad3026afc09e69e3a25",
+    "url": "/007/static/css/main.506b122e.chunk.css"
+  },
+  {
+    "revision": "ededaf3145e1202a3ecd",
+    "url": "/007/static/js/2.6feea587.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/007/static/js/2.6feea587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad3026afc09e69e3a25",
+    "url": "/007/static/js/main.fa0a488d.chunk.js"
+  },
+  {
+    "revision": "2da443d40399f20e3a24",
+    "url": "/007/static/js/runtime-main.af37b461.js"
+  }
+]);
